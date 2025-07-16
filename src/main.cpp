605ef443922f122d2ee204543de9fd5e2133b710
nua <enuesaa@gmail.com>
2025-07-16 20:25:27 +0900

    feat: ボタンを押した時に反応するようにした

--- src/main.cpp
+++ src/main.cpp
@@ -9,34 +9,48 @@
 void setup() {
   M5.begin();
   M5.Lcd.setTextSize(3);
-  M5.Lcd.print("start!");
+  M5.setTouchButtonHeight(32);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  // M5.Lcd.print("start!");
 
-  for (int i = 0; i < 5; i++) {
-    if (WiFi.status() == WL_CONNECTED) {
-      break;
-    }
-    delay(500);
-  }
+  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  if (WiFi.status() != WL_CONNECTED) {
-    M5.Lcd.print("failed. status is ");
-    M5.Lcd.println(WiFi.status());
-    return;
-  }
-  M5.Lcd.print("connected. local ip is ");
-  M5.Lcd.println(WiFi.localIP());
+  // for (int i = 0; i < 5; i++) {
+  //   if (WiFi.status() == WL_CONNECTED) {
+  //     break;
+  //   }
+  //   delay(500);
+  // }
+
+  // if (WiFi.status() != WL_CONNECTED) {
+  //   M5.Lcd.print("failed. status is ");
+  //   M5.Lcd.println(WiFi.status());
+  //   return;
+  // }
+  // M5.Lcd.print("connected. local ip is ");
+  // M5.Lcd.println(WiFi.localIP());
   
-  HTTPClient http;
+  // HTTPClient http;
     
-  http.begin("https://example.com/aaa");
-  int statusCode = http.GET();
-  M5.Lcd.print("status:");
-  M5.Lcd.println(statusCode);
+  // http.begin("https://example.com/aaa");
+  // int statusCode = http.GET();
+  // M5.Lcd.print("status:");
+  // M5.Lcd.println(statusCode);
 
-  http.end();
-  WiFi.disconnect(true);
+  // http.end();
+  // WiFi.disconnect(true);
 }
 
-void loop() {}
+void loop() {
+  M5.delay(1);
+  M5.update();
+
+  int w = M5.Display.width() / 5;
+  int h = M5.Display.height();
+  M5.Display.startWrite();
+
+  if (M5.BtnA.wasClicked()) {
+    M5.Display.fillRect(w*1, 0, w-1, h, TFT_RED);
+  }
+  M5.Display.endWrite();
+}
